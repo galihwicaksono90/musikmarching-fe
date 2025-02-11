af0e17273e9f51186ad1e8bb202340e6e7b34acc
Galih Wicaksono <galihwicaksono90@gmail.com>
2025-02-11 17:51:59 +0700

    fix contributor score card list

--- src/routes/(app)/(logged-in)/contributor/+page.svelte
+++ src/routes/(app)/(logged-in)/contributor/+page.svelte
@@ -3,46 +3,77 @@
 	import { Button } from "$lib/components/ui";
 	import * as Card from "$lib/components/ui/card";
 	import { ContributorRegisterForm } from "$lib/components/form";
+	import type { ContributorScore } from "$lib/model";
+	import { VerifiedStatusBadge } from "$lib/components/common";
+
+	import { AudioPlayer, ScoreImageCarousel } from "$lib/components/common";
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-{#if !data.user?.is_verified}
-	<div >
-		<div class="w-full border-2 rounded-xl shadow-md p-4 bg-green-700 mb-8">
-			<h3>
-				Your account is not verified yet. Please wait for the admin to verify
-				your account
-			</h3>
-		</div>
-		<Card.Root class="max-w-xl mx-auto">
-			<Card.Header>
-				<Card.Title>Your Contributor Profile</Card.Title>
-				<Card.Description>
-					Please fill in your profile to become a contributor
-				</Card.Description>
-			</Card.Header>
-			<Card.Content>
-				<ContributorRegisterForm data={data.form} />
-			</Card.Content>
-		</Card.Root>
-	</div>
-{:else}
-	<div class="flex gap-8">
-		{#each data.scores as score}
-			<a href={`/contributor/score/${score.id}`}>
-				<div class="w-32 flex gap-8 w-full border-2 rounded-xl shadow-md p-8">
-					<div class="flex flex-col">
-						<p>{score.title}</p>
-						<p>{score.price}</p>
-						<p>{String(score.is_verified)}</p>
-						<button> View </button>
-					</div>
+{#snippet contributorScoreCard({ score }: { score: ContributorScore })}
+	<Card.Root class="h-full w-full">
+		<Card.Header>
+			<div class="flex flex-col">
+				<div class="flex flex-row justify-between -center min-h-8">
+					<Card.Title>{score.title}</Card.Title>
+					<VerifiedStatusBadge isVerified={score.is_verified} />
 				</div>
+				<Card.Description>{score.description}</Card.Description>
+			</div>
+		</Card.Header>
+		<Card.Content>
+			<div>
+				<ScoreImageCarousel images={score.pdf_image_urls} />
+				<AudioPlayer src={score.audio_url} />
+			</div>
+		</Card.Content>
+		<Card.Footer class="flex justify-end">
+			<a href={`/contributor/score/${score.id}`}>
+				<Button>View</Button>
 			</a>
-		{/each}
+		</Card.Footer>
+	</Card.Root>
+{/snippet}
+
+<section class="flex flex-col gap-8">
+	<div class="flex flex-row justify-between w-full">
+		<h2
+			class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+		>
+			Daftar Score
+		</h2>
+		<a href="/contributor/score/create">
+			<Button>Create Score</Button>
+		</a>
 	</div>
-	<a href="/contributor/score/create">
-		<Button>Create Score</Button>
-	</a>
-{/if}
+	{#if !data.user?.is_verified}
+		<div>
+			<div class="w-full border-2 rounded-xl shadow-md p-4 bg-green-700 mb-8">
+				<h3>
+					Your account is not verified yet. Please wait for the admin to verify
+					your account
+				</h3>
+			</div>
+			<Card.Root class="max-w-xl mx-auto">
+				<Card.Header>
+					<Card.Title>Your Contributor Profile</Card.Title>
+					<Card.Description>
+						Please fill in your profile to become a contributor
+					</Card.Description>
+				</Card.Header>
+				<Card.Content>
+					<ContributorRegisterForm data={data.form} />
+				</Card.Content>
+			</Card.Root>
+		</div>
+	{:else}
+		<div class="flex flex-col gap-8">
+			<div class="grid grid-cols-3 min-h-[16rem] gap-8">
+				{#each data.scores as score}
+					{@render contributorScoreCard({ score })}
+				{/each}
+			</div>
+		</div>
+	{/if}
+</section>
